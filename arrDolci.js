afb609ac2d24713ee1e5b7f0bdf3f86dd98158e6
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-12-02 13:57:56 +0100

    feat: ora arriva la parte divertente

---
+++ arrDolci.js
@@ -0,0 +1,37 @@
+const arrayTag = ["Dolce", "Salato"];
+
+// Creo un file seprato l'array di oggetti contenente i dati di ogni dolce.
+const arrayRicette = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "Non c'è contenuto",
+    immagine: "./public/ciambellone.jpeg",
+    tags: arrayTag[0],
+  },
+  {
+    titolo: "Cracker Barbabietola",
+    contenuto: "Non c'è contenuto",
+    immagine: "./public/ciambellone.jpeg",
+    tags: arrayTag[1],
+  },
+  {
+    titolo: "Pane fritto dolce",
+    contenuto: "Non c'è contenuto",
+    immagine: "./public/ciambellone.jpeg",
+    tags: arrayTag[0],
+  },
+  {
+    titolo: "Pasta barbabietola",
+    contenuto: "Non c'è contenuto",
+    immagine: "./public/ciambellone.jpeg",
+    tags: arrayTag[1],
+  },
+  {
+    titolo: "Torta paesana",
+    contenuto: "Non c'è contenuto",
+    immagine: "./public/ciambellone.jpeg",
+    tags: arrayTag[0],
+  },
+];
+
+module.exports = { arrayRicette };
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// console.log("Ciaone");
+// Importo express
+const express = require("express");
+// Asseggno ad app l'oggetto ottenuto richiamando il metodo express
+const app = express();
+// Creo una porta dove mettere in ascolto il server
+const port = 3000;
+
+// Importo l'array di ricette
+const { arrayRicette } = require("./arrDolci.js");
+console.log(arrayRicette);
+
+// Setto come pubblica la cartella public per poterci accedere
+app.use(express.static("public"));
+
+app.get("/", (request, respsonse) => {
+  console.log("Server del mio blog");
+});
+
+app.get("/bacheca", (request, respsonse) => {
+  console.log("Bello zzzio");
+});
+
+app.listen(port, () => {
+  console.log("Sono in ascolto bro");
+});
